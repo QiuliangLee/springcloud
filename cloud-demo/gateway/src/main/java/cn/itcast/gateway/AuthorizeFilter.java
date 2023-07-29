fb161374b4f4d95c099fac812d791f7314c623cf
liqiuliang <liqiuliang@bilibili.com>
2023-07-29 19:19:52 +0800

    feat:add rabbitMq, es

--- cloud-demo/gateway/src/main/java/cn/itcast/gateway/AuthorizeFilter.java
+++ cloud-demo/gateway/src/main/java/cn/itcast/gateway/AuthorizeFilter.java
@@ -2,7 +2,7 @@ package cn.itcast.gateway;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
@@ -10,9 +10,9 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-// @Order(-1)
+@Order(-1)
 @Component
-public class AuthorizeFilter implements GlobalFilter, Ordered {
+public class AuthorizeFilter implements GlobalFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         // 1.获取请求参数
@@ -30,10 +30,5 @@ public class AuthorizeFilter implements GlobalFilter, Ordered {
         exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
         // 5.2.拦截请求
         return exchange.getResponse().setComplete();
-    }
-
-    @Override
-    public int getOrder() {
-        return -1;
     }
 }
